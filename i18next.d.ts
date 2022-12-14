34d7eed8b9b4f646b6ab77fc7abbb063a08dd0dd
Rodrigo J Moura <rodrigojorgemoura@gmail.com>
2022-12-14 02:01:26 +0000

    Update i18next.d.ts

--- i18next.d.ts
+++ i18next.d.ts
@@ -1,14 +1,6 @@
 import "i18next";
 import locales from "./public/locales";
 
-import type common from "./public/locales/en/common.json";
-import type extra from "./public/locales/en/extra.json";
-
-interface I18nNamespaces {
-  common: typeof common;
-  extra: typeof extra;
-}
-
 declare module "i18next" {
   interface CustomTypeOptions {
     defaultNS: "common";
